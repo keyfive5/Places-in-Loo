5ff452b8be7d2639982779da043b95b92b5604a1
Shahyan Muhammad Khan <khan6270@mylaurier.ca>
2022-07-15 18:57:01 -0400

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MainMenu extends JFrame{
+public class MainMenu extends JFrame implements ActionListener{
     JFrame frame = new JFrame();
     JButton postButton = new JButton("Post a Sublet");
     JButton rentButton = new JButton("Rent a Sublet");
@@ -14,7 +14,7 @@ public class MainMenu extends JFrame{
         // Labels and Text Fields
         postButton.setBounds(100,160,200,60);
         postButton.setFocusable(false);
-        //postButton.addActionListener(this);
+        postButton.addActionListener(this);
 
         rentButton.setBounds(320,160,200,60);
         rentButton.setFocusable(false);
@@ -30,11 +30,28 @@ public class MainMenu extends JFrame{
         frame.add(cancelButton);
         frame.add(rateButton);
         
+        frame.getContentPane().setBackground(new Color(173, 216, 230));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
         frame.setVisible(true);
 
+    }
+    public void actionPerformed(ActionEvent e) {
+  
+        if(e.getSource()==postButton) {
+         frame.dispose();
+         //NewWindow myWindow = new NewWindow();
+        }
+        else if(e.getSource()==rentButton){
+            frame.dispose();
+        }
+        else if(e.getSource()==cancelButton){
+            frame.dispose();
+        }
+        else if(e.getSource()==rateButton){
+            frame.dispose();
+        }
     }
 
     public static void main(String[] args) {
