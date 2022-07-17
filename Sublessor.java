56cd7c6b5c552cdcbb22c943ca6d754663b1ffe3
jackli10345 <35533992+jackli10345@users.noreply.github.com>
2022-07-17 12:36:02 -0400

    Add files via upload

--- Sublessor.java
+++ Sublessor.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 public class Sublessor {
 	public ArrayList<Posting> postings = new ArrayList<Posting>();
 	
-	public void createPosting() {
+	public void createPosting(String location, Sublessor sublessor, int duration, String description, int post_id) {
 		//TODO
+		Posting posting = new Posting(location, sublessor, duration, description, post_id);
+		postings.add(posting);
 	}
 	
-	public void deletePosting(post_id ID) {
+	public void deletePosting(int post_id) {
 		for(int i = 0; i < postings.length(); i++) {
-			if(postings.get(i).getID() == ID) {
+			if(postings.get(i).getID() == post_id) {
 				postings.remove(i);
 			}
 		}
