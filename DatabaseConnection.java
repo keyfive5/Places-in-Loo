61a71be534b5428c76097460659effeab9fb5b28
Rhinoceroach <whyamiso24.7@gmail.com>
2022-07-21 14:33:56 -0400

    Fixed DatabaseConnection functionality

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -2,17 +2,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 // Connector class that runs SQL queries through a database
 public class DatabaseConnection {
 
+    // Set this parameter to file location of of the database
     private String database;
 
     public DatabaseConnection(String database){
         this.database = database;
     }
 
-    public ResultSet retrieveQuery(String query) {
+    public String[] retrieveQuery(String query) {
         Connection conn = null;
         try {
             // db parameters
@@ -25,15 +27,23 @@ public class DatabaseConnection {
             Statement statement = conn.createStatement();
 
             ResultSet rs = statement.executeQuery(query);
-            rs.next();
-            String name = rs.getString("username");
-            System.out.println(name);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columns = rsmd.getColumnCount();
+
+            String[] result = new String[columns];
+            while (rs.next()){
+                for (int i=0;i<columns;i++){
+                    result[i] = rs.getString(i+1);
+                }
+            }
+            
+            //String name = rs.getString("username");
 
             if (conn != null) {
                 conn.close();
             }
 
-            return rs;
+            return result;
             
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -43,6 +53,8 @@ public class DatabaseConnection {
         return null;
     }
 
+
+    // Enter the update query, returns True upon success
     public Boolean updateQuery(String query){
         Connection conn = null;
         try {
@@ -66,4 +78,14 @@ public class DatabaseConnection {
             return false;
         }
     }
+
+    //public Boolean insert(String tablename, String values){
+    //    String query = "INSERT INTO " + tablename + " VALUES(" + values + ")";
+    //    return this.updateQuery(query);
+    //}
+//
+    //public ResultSet retrieve(String fields, String tablename, String arguments){
+    //    String query = "SELECT " + fields + " FROM "
+    //    return ;
+    //}
 }
