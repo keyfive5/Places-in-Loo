e42900364df01d08a04883d67c337cdd783adda1
Rhinoceroach <whyamiso24.7@gmail.com>
2022-07-21 14:40:16 -0400

    Added documentation to ConnectionExample

--- ConnectionExample.java
+++ ConnectionExample.java
@@ -1,8 +1,15 @@
 public class ConnectionExample {
     public static void main(String[] args){
+        // Object used to connect to the database
         DatabaseConnection conn = new DatabaseConnection("Places-in-Loo\\data.db");
-        conn.updateQuery("INSERT INTO USER VALUES(null,'username','password','email@mylaurier.ca','firstname','lastname','gender','university','2001-11-02');");
+        
+        // Insert values into table example
+        //conn.updateQuery("INSERT INTO USER VALUES(null,'username','password','email@mylaurier.ca','firstname','lastname','gender','university','2001-11-02');");
+        
+        // Select query example to retrieve data
         String[] result = conn.retrieveQuery("SELECT * FROM USER WHERE username='username';");
+        
+        // Retrieve query returns list of strings, can find all field values in these strings
         for (int i=0;i<result.length;i++){
             System.out.println(result[i]);
         }
