f34cca349d391630454d5ad0b4aae523983b9bc5
Rhinoceroach <whyamiso24.7@gmail.com>
2022-07-24 15:19:50 -0400

    Fixed Database Connection dependencies

--- ConnectionExample.java
+++ ConnectionExample.java
@@ -1,13 +1,13 @@
 public class ConnectionExample {
     public static void main(String[] args){
         // Object used to connect to the database
-        DatabaseConnection conn = new DatabaseConnection("Places-in-Loo\\data.db");
+         DatabaseConnection conn = new DatabaseConnection("Places-in-Loo\\data.db");
         
         // Insert values into table example
         //conn.updateQuery("INSERT INTO USER VALUES(null,'username','password','email@mylaurier.ca','firstname','lastname','gender','university','2001-11-02');");
         
         // Select query example to retrieve data
-        String[] result = conn.retrieveQuery("SELECT * FROM USER WHERE username='username';");
+        String[] result = conn.retrieveQuery("SELECT * FROM USER WHERE username='shai1640';");
         
         // Retrieve query returns list of strings, can find all field values in these strings
         for (int i=0;i<result.length;i++){
