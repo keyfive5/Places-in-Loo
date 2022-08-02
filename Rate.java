1b64010f570f1ed16acf90ab9e0875688b1d4cc3
Wrenen <105389231+Wrenen@users.noreply.github.com>
2022-08-02 01:07:20 -0400

    Update Rate.java

--- Rate.java
+++ Rate.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class Rate {
     private int rating = 0;
+    
     Rate(){
-
         Icon star = new ImageIcon("./assets/star.png");
         JButton btnRate1 = new JButton(star);
         JButton btnRate2 = new JButton(star);
@@ -13,6 +13,7 @@ public class Rate {
         JButton btnRate4 = new JButton(star);
         JButton btnRate5 = new JButton(star);
         JLabel lblQuest =new JLabel("How would you like to rate your pervious sublet?");
+        JLabel lblCurrent = new JLabel("Current rating: " + rating + "/5");
         JButton btnSubmit = new JButton("Submit");
         JTextField txtReview = new JTextField("(optional) Enter a review");
 
@@ -28,55 +29,64 @@ public class Rate {
         lblQuest.setFont(new Font(null,Font.PLAIN,15));
         panel.add(lblQuest);
 
-        btnRate1.setBounds(80,80,32,32);
+        btnRate1.setBounds(80,60,32,32);
         btnRate1.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 rating = 1;
+                lblCurrent.setText("Current rating: " + rating + "/5");
             }
         });
         panel.add(btnRate1);
 
-        btnRate2.setBounds(130,80,32,32);
+        btnRate2.setBounds(130,60,32,32);
         btnRate2.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 rating = 2;
+                lblCurrent.setText("Current rating: " + rating + "/5");
             }
         });
         panel.add(btnRate2);
 
-        btnRate3.setBounds(180,80,32,32);
+        btnRate3.setBounds(180,60,32,32);
         btnRate3.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 rating = 3;
+                lblCurrent.setText("Current rating: " + rating + "/5");
             }
         });
         panel.add(btnRate3);
 
-        btnRate4.setBounds(230,80,32,32);
+        btnRate4.setBounds(230,60,32,32);
         btnRate4.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 rating = 4;
+                lblCurrent.setText("Current rating: " + rating + "/5");
             }
         });
         panel.add(btnRate4);
 
-        btnRate5.setBounds(280,80,32,32);
+        btnRate5.setBounds(280,60,32,32);
         btnRate5.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 rating = 5;
+                lblCurrent.setText("Current rating: " + rating + "/5");
             }
         });
         panel.add(btnRate5);
+
+        lblCurrent.setBounds(160,93,500,25);
+        lblCurrent.setFont(new Font(null,Font.PLAIN,11));
+        panel.add(lblCurrent);
 
         txtReview.setBounds(20,125,360,180);
         panel.add(txtReview);
@@ -86,7 +96,14 @@ public class Rate {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(rating +" stars");
+                if(rating == 0){
+                    JOptionPane.showMessageDialog(frame, "Select a rating from 1 - 5");
+                }
+                DatabaseConnection conn = new DatabaseConnection();
+                float avgRating;
+                avgRating = rating;
+
+                
                 if (new String("(optional) Enter a review").equals(txtReview.getText())){
                     return;
                 } else {
