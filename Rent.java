aeba76393bf9def08e514ff4e2069e3ea9b26e42
Wrenen <105389231+Wrenen@users.noreply.github.com>
2022-08-03 08:32:19 -0400

    Update Rent.java

--- Rent.java
+++ Rent.java
@@ -2,19 +2,8 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Rent {
-    private JFrame frame = new JFrame();
-    private JLabel label = new JLabel("Test");
     
     Rent(){
-    
-    label.setBounds(0,0,100,50);
-    label.setFont(new Font(null,Font.PLAIN,25));
-    
-    frame.add(label);
-    
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(420,420);
-    frame.setLayout(null);
-    frame.setVisible(true);
+        sublet_page Menu = new sublet_page();
     }
 }
