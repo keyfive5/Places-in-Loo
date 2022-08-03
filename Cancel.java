e608509129b86e29f3f59369d2a9f680b4395540
Wrenen <105389231+Wrenen@users.noreply.github.com>
2022-08-03 08:31:03 -0400

    Update Cancel.java

--- Cancel.java
+++ Cancel.java
@@ -1,19 +1,49 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+
 public class Cancel {
-JFrame frame = new JFrame();
- JLabel label = new JLabel("Test");
- 
- Cancel(){
-  
-  label.setBounds(0,0,100,50);
-  label.setFont(new Font(null,Font.PLAIN,25));
-  
-  frame.add(label);
-  
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  frame.setSize(420,420);
-  frame.setLayout(null);
-  frame.setVisible(true);
- }
+    private JFrame frame = new JFrame();
+    private JLabel label = new JLabel("Are sure you want to cancel your current sublet?");
+    private JButton cancel_btn = new JButton("Cancel");
+    private Icon home = new ImageIcon("./Assets/home.png");
+    private JButton home_btn = new JButton(home);
+    
+    Cancel(){
+        home_btn.setBounds(5,5,32,32);
+        home_btn.setBorder(null);
+        home_btn.setBackground(null);
+        home_btn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                MainMenu Menu = new MainMenu();
+            }
+        });
+        frame.add(home_btn);    
+    
+        label.setBounds(20,30,400,100);
+        label.setFont(new Font(null,Font.PLAIN,25));
+
+        cancel_btn.setBounds(100,80,200,60);
+        cancel_btn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Your sublet has been canceled.");
+            }
+        });
+        
+        frame.add(label);
+        frame.add(cancel_btn);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(173, 216, 230));
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    return;
+    }
 }
