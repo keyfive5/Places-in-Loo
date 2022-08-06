24d29ef5c8db2aa282ed88277ff90bfe59affb8f
Rhinoceroach <whyamiso24.7@gmail.com>
2022-08-06 15:28:23 -0400

    Update Contact

--- contact.java
+++ contact.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class contact extends JFrame implements ActionListener{
+public class Contact {
     JFrame frame = new JFrame();
     JLabel title = new JLabel("Message");
     JButton send = new JButton("Send");
@@ -10,12 +10,8 @@ public class contact extends JFrame implements ActionListener{
     JTextField send_to = new JTextField("TO:");
     JTextArea message = new JTextArea("Message:");
 
-    J
-    
-
-
     // Intialize
-    contact() {
+    public Contact() {
         title.setFont(new Font("Calibri", Font.BOLD, 45));
         title.setBounds(120,10,1000,60);
         title.setFocusable(false);
@@ -26,6 +22,12 @@ public class contact extends JFrame implements ActionListener{
         message.setBounds(20, 100, 350, 200);
 
         send.setBounds(150,320,100,40);
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Message sent!");
+            }
+        });
         back.setBounds(275, 320, 100, 40);
        
         frame.add(title);
@@ -40,12 +42,9 @@ public class contact extends JFrame implements ActionListener{
         frame.setVisible(true);
 
     }
-    public void actionPerformed(ActionEvent e) {
-  
-    }
 
     public static void main(String[] args) {
-        contact Menu = new contact();
+        Contact contact_menu = new Contact();
     }
 
 }
